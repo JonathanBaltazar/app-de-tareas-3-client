b1a50674310b001a6f3da2a7b8c61270cd1aa73f
jonathan <jorgejonatthan@gmail.com>
2023-09-15 18:07:21 -0600

    direccion ip añadida

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let instance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "http://191.96.31.90:3000/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
